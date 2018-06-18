1c23008e454960e1081309417969281f8288c3e7
Daniel <danielghazi00@gmail.com>
2018-06-18 11:17:43 +0300

    init

---
+++ Blackjack/Game.xaml.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using MySql.Data.MySqlClient;
+
+namespace Blackjack
+{
+    /// <summary>
+    /// Interaction logic for Game.xaml
+    /// </summary>
+    public partial class Game : Page
+    {
+        private const string connectionString = "server=localhost;user=root;database=game_21;port=3306;password=root";
+        private const string create_game = "create_game";
+        private const string add_player_to_game = "add_player_to_game";
+        private const string start_game = "start_game";
+        private const string get_chosen_player_hand_id = "get_chosen_player_hand_id";
+        private const string get_hand_cards_ids = "get_hand_cards_ids";
+
+        private readonly int _loginedPlayerId;
+        private readonly int _playerHandId;
+        private List<int> _playersCards;
+
+        private const int _dealerId = 11;
+        private readonly int _dealerHandId;
+
+        private int _gameId;
+
+        public Game()
+        {
+            InitializeComponent();
+        }
+
+        public Game(int loginedPlayerId) : this()
+        {
+            _loginedPlayerId = loginedPlayerId;
+
+            CreateGame();
+            AddDealerToGame();
+            StartGame();
+            _playerHandId = GetChosenPlayerHandId(_loginedPlayerId);
+            _dealerHandId = GetChosenPlayerHandId(_dealerId);
+            GetHandCardsIds(_playerHandId);
+            GetHandCardsIds(_dealerHandId);
+        }
+
+        private void CreateGame()
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                MySqlCommand cmd = new MySqlCommand(create_game, connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@_game_host_id", _loginedPlayerId);
+                cmd.Parameters["@_game_host_id"].Direction = ParameterDirection.Input;
+
+                cmd.Parameters.AddWithValue("@_game_id", MySqlDbType.Int32);
+                cmd.Parameters["@_game_id"].Direction = ParameterDirection.Output;
+
+                cmd.ExecuteNonQuery();
+
+                _gameId = (int)cmd.Parameters["@_game_id"].Value;
+            }
+        }
+
+        private void AddDealerToGame()
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                MySqlCommand cmd = new MySqlCommand(add_player_to_game, connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@_game_id", _gameId);
+                cmd.Parameters["@_game_id"].Direction = ParameterDirection.Input;
+
+                cmd.Parameters.AddWithValue("@_player_id", _dealerId);
+                cmd.Parameters["@_player_id"].Direction = ParameterDirection.Input;
+
+                cmd.Parameters.AddWithValue("@_result_status", MySqlDbType.Int32);
+                cmd.Parameters["@_result_status"].Direction = ParameterDirection.Output;
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void StartGame()
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                MySqlCommand cmd = new MySqlCommand(start_game, connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@_game_id", _gameId);
+                cmd.Parameters["@_game_id"].Direction = ParameterDirection.Input;
+
+                cmd.Parameters.AddWithValue("@_result_status", MySqlDbType.Int32);
+                cmd.Parameters["@_result_status"].Direction = ParameterDirection.Output;
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private int GetChosenPlayerHandId(int playerId)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                MySqlCommand cmd = new MySqlCommand(get_chosen_player_hand_id, connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@_game_id", _gameId);
+                cmd.Parameters["@_game_id"].Direction = ParameterDirection.Input;
+
+                cmd.Parameters.AddWithValue("@_player_id", playerId);
+                cmd.Parameters["@_player_id"].Direction = ParameterDirection.Input;
+
+                return (int)cmd.ExecuteScalar();
+            }
+        }
+
+        private List<int> GetHandCardsIds(int handId)
+        {
+            var handCardsIds = new List<int>();
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                MySqlCommand cmd = new MySqlCommand(get_hand_cards_ids, connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@_hand_id", handId);
+                cmd.Parameters["@_hand_id"].Direction = ParameterDirection.Input;
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    handCardsIds.Add((int)reader["card_id"]);
+                }
+            }
+
+            return handCardsIds;
+        }
+
+        private void TakeCard_Click(object sender, RoutedEventArgs e)
+        {
+            // GetHand
+            // CheckTakenCardsAmount
+            // GetTopCardFromDeck (ИСПОЛЬЗОВАТЬ ТРИГГЕР ДЛЯ АВТОУДАЛЕНИЯ КАРТЫ) (ПЕРЕДАВАТЬ HAND_ID И ПРОВЕРЯТЬ, ЧТО КОЛОДА НЕ ПЕРЕПОЛНЕНА. 
+            // ДОБАВЛЯТЬ КАРТУ В РУКУ В ЭТОЙ ЖЕ ПРОЦЕДУРЕ)
+            // GetHand
+            // ShowCards
+            // UpdateSumText
+        }
+
+        private void MainMenu_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("MainMenu.xaml", UriKind.Relative));
+        }
+    }
+}
---
+++ Blackjack/MainMenu.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Blackjack
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Page
+    {
+        private const string connectionString = "server=localhost;user=root;database=game_21;port=3306;password=root";
+        private const string register_new_player = "register_new_player";
+        private const string login = "login";
+
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private enum ResultStatus
+        {
+            Success,
+            Error
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            ResultStatus registrationResult;
+            int playerId;
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    MySqlCommand cmd = new MySqlCommand(register_new_player, connection);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@_name", UserNameInput.Text);
+                    cmd.Parameters["@_name"].Direction = ParameterDirection.Input;
+
+                    cmd.Parameters.AddWithValue("@_result_status", MySqlDbType.Int32);
+                    cmd.Parameters["@_result_status"].Direction = ParameterDirection.Output;
+
+                    cmd.Parameters.AddWithValue("@_player_id", MySqlDbType.Int32);
+                    cmd.Parameters["@_player_id"].Direction = ParameterDirection.Output;
+
+                    cmd.ExecuteNonQuery();
+
+                    registrationResult = (ResultStatus) cmd.Parameters["@_result_status"].Value;
+                    playerId = (int) cmd.Parameters["@_player_id"].Value;
+                }
+            }
+            catch
+            {
+                ErrorText.Text = "Login's length should be from 3 to 50 characters.";
+                return;
+            }
+
+            if (registrationResult == ResultStatus.Success)
+            {
+                var game = new Game(playerId);
+                NavigationService.Navigate(game);
+            }
+            else
+            {
+                ErrorText.Text = "Player with given name already exists.";
+            }
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            ResultStatus loginResult;
+            int playerId;
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                MySqlCommand cmd = new MySqlCommand(login, connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@_name", UserNameInput.Text);
+                cmd.Parameters["@_name"].Direction = ParameterDirection.Input;
+
+                cmd.Parameters.AddWithValue("@_result_status", MySqlDbType.Int32);
+                cmd.Parameters["@_result_status"].Direction = ParameterDirection.Output;
+
+                cmd.Parameters.AddWithValue("@_player_id", MySqlDbType.Int32);
+                cmd.Parameters["@_player_id"].Direction = ParameterDirection.Output;
+
+                cmd.ExecuteNonQuery();
+
+                loginResult = (ResultStatus)cmd.Parameters["@_result_status"].Value;
+                playerId = (int)cmd.Parameters["@_player_id"].Value;
+            }
+
+            if (loginResult == ResultStatus.Success)
+            {
+                var game = new Game(playerId);
+                NavigationService.Navigate(game);
+            }
+            else
+            {
+                ErrorText.Text = "Player with given name does not exist.";
+            }
+        }
+    }
+}
